8ae7c4cc7fbe63249576b1145b63118c74f0e0b2
MarcusDLG <delag010@gmail.com>
2020-03-16 13:58:23 -0400

    part 1 complete

--- public/main.js
+++ public/main.js
@@ -4,4 +4,21 @@ const main = () => {
   }
 }
 
+const updateTeamName = (teamInput, teamName) => {
+  const nameToChange = document.querySelector(teamInput).value
+  document.querySelector(teamName).textContent = nameToChange
+}
+
+const teamOne = () => {
+  updateTeamName('.team-1-input', '.team-1-name')
+}
+
+const teamTwo = () => {
+  updateTeamName('.team-2-input', '.team-2-name')
+}
+
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.update-team-1-name').addEventListener('click', teamOne)
+
+document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.update-team-2-name').addEventListener('click', teamTwo)
